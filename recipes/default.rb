283b0ba12dc62952ee67c701520b23c73a4be7b6
Sebastian Boehm <sebastian@sometimesfood.org>
2012-04-16 19:48:36 +0200

    Configure avahi-daemon

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,16 @@
 
 package 'netatalk'
 package 'avahi-daemon'
+
+%w(netatalk avahi-daemon).each do |s|
+  service s do
+    supports :restart => true
+    action :nothing
+  end
+end
+
+cookbook_file '/etc/avahi/services/afpd.service' do
+  mode '644'
+  notifies :restart, 'service[avahi-daemon]'
+  notifies :restart, 'service[netatalk]'
+end
